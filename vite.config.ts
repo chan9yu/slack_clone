38fc59bbdd91e3edba79d60f7ec8a053ab1617db
chan9yu <wry5533@gmail.com>
2023-10-01 00:24:51 +0900

    feat: emotion 플러그인 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react({
+			jsxImportSource: '@emotion/react',
+			babel: {
+				plugins: [
+					[
+						'@emotion/babel-plugin',
+						{
+							autoLabel: 'dev-only',
+							labelFormat: '[local]'
+						}
+					]
+				]
+			}
+		})
+	],
 	base: '/test/',
 	server: {
 		host: 'localhost',
