83b29483868e09a85495fbccab60977ffdc8e5d9
chan9yu <wry5533@gmail.com>
2023-10-02 12:08:55 +0000

    Updates

--- storybook/assets/Typography.stories-58f2582b.js
+++ storybook/assets/Typography.stories-58f2582b.js
@@ -1,4 +1,4 @@
-import{M as l,T as s}from"./index-83c1c10b.js";import{t as o}from"./typography-18d5d997.js";import{j as t}from"./jsx-runtime-ffb262ed.js";import{u as p}from"./index-a1cf9e47.js";import"./iframe-42ff0c0c.js";import"../sb-preview/runtime.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-15b115a7.js";import"./index-ffc7e5ff.js";import"./index-d37d4223.js";import"./index-356e4a49.js";function r(n){const e=Object.assign({h1:"h1",ul:"ul",li:"li",h3:"h3"},p(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(l,{title:"Foundation/Typography"}),`
+import{M as l,T as s}from"./index-fee85812.js";import{t as o}from"./typography-18d5d997.js";import{j as t}from"./jsx-runtime-ffb262ed.js";import{u as p}from"./index-a1cf9e47.js";import"./iframe-76e576f1.js";import"../sb-preview/runtime.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-15b115a7.js";import"./index-ffc7e5ff.js";import"./index-d37d4223.js";import"./index-356e4a49.js";function r(n){const e=Object.assign({h1:"h1",ul:"ul",li:"li",h3:"h3"},p(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(l,{title:"Foundation/Typography"}),`
 `,t.jsx(e.h1,{id:"typography",children:"Typography"}),`
 `,t.jsxs(e.ul,{children:[`
 `,t.jsx(e.li,{children:"typography 정의 내용입니다."}),`
@@ -12,4 +12,4 @@ import{M as l,T as s}from"./index-83c1c10b.js";import{t as o}from"./typography-1
 `,t.jsx(s,{fontSizes:["caption2","caption1","subheading","body","heading4","heading3"].map(a=>o.size[a]),fontWeight:o.weight.bold,sampleText:"Bold",fontFamily:"'Lato', sans-serif"}),`
 `,t.jsx(e.h3,{id:"lato---black",children:"Lato - Black"}),`
 `,t.jsx(s,{fontSizes:Object.values(o.size),fontWeight:o.weight.black,sampleText:"Black",fontFamily:"'Lato', sans-serif"})]})}function m(n={}){const{wrapper:e}=Object.assign({},p(),n.components);return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r(n)}const d=()=>{throw new Error("Docs-only story")};d.parameters={docsOnly:!0};const i={title:"Foundation/Typography",tags:["stories-mdx"],includeStories:["__page"]};i.parameters=i.parameters||{};i.parameters.docs={...i.parameters.docs||{},page:m};const z=["__page"];export{z as __namedExportsOrder,d as __page,i as default};
-//# sourceMappingURL=Typography.stories-0d44bbce.js.map
+//# sourceMappingURL=Typography.stories-58f2582b.js.map
--- storybook/assets/syntaxhighlighter-PONEFOSF-ca0b32e0.js
+++
@@ -1,2 +0,0 @@
-import{S as l,c as n,s as d}from"./index-83c1c10b.js";import"./iframe-42ff0c0c.js";import"../sb-preview/runtime.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-15b115a7.js";import"./index-ffc7e5ff.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
-//# sourceMappingURL=syntaxhighlighter-PONEFOSF-ca0b32e0.js.map
