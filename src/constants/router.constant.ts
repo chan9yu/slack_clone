81290eeb76e88bf94574eda6ef8be8c95c254ee5
chan9yu <wry5533@gmail.com>
2023-10-02 19:25:24 +0900

    fix: github page 새로고침 후 404 페이지 나오는 이슈
    - hash router로 변경

--- src/constants/router.constant.ts
+++ src/constants/router.constant.ts
@@ -1,10 +1,8 @@
-export const BASE_URL = '/slack_clone/';
-
 export enum ROUTER_PATH {
-	ROOT = `${BASE_URL}`,
+	ROOT = '/',
 	// AUTH PAGES
-	AUTH = `${BASE_URL}auth/`,
-	AUTH_LOGIN = `${BASE_URL}auth/login`,
-	AUTH_SIGNUP = `${BASE_URL}auth/signup`
+	AUTH = `/auth/`,
+	AUTH_LOGIN = `/auth/login`,
+	AUTH_SIGNUP = `/auth/signup`
 	//
 }
